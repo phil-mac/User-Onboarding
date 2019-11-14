6d34158cebd69745f9a5ebf1b183070281807e2a
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2019-11-14 13:24:57 -0800

    implemented form validation and post request

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {withFormik, Form, Field} from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 
-const UserForm = () => {
+const UserForm = ({values, errors, touched, status}) => {
+    const [response, setResponse] = useState({});
+
+    useEffect(() => {
+        status && setResponse(status)
+    }, [status])
+
     return(
         <Form>
             <Field type='text' name='name' placeholder='Name'/>
+            {touched.name && errors.name && <span>{errors.name}</span>}
+            <br />
             <Field type='email' name='email' placeholder='Email'/>
+            {touched.email && errors.email && <span>{errors.email}</span>}
+            <br />
             <Field type='password' name='password' placeholder='Password'/>
+            {touched.password && errors.password && <span>{errors.password}</span>}
+            <br />
             <label>
+                <Field type='checkbox' name='tos' checked={values.tos}/>
                 Sign all your rights away.
-                <Field type='checkbox' name='tos'/>
             </label>
+            <br />
             <button type='submit'>Submit</button>
         </Form>
         
@@ -25,5 +40,19 @@ export default withFormik({
             password: password || '',
             tos: tos || true
         }
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().min(4,'Name must be at least 4 characters.').required('Name is required.'),
+        email: Yup.string().email('Email must be valid.').required('Email is required.'),
+        password: Yup.string().min(8,'Password must be at least 8 characters.').required('Password is required.')
+    }),
+    handleSubmit(values, {setStatus}){
+        axios
+            .post('https://reqres.in/api/users', values)
+            .then(res => {
+                setStatus(res.data);
+                console.log(res);
+            })
+            .catch(err => console.log("oops..." + err.message));
     }
 })(UserForm)
