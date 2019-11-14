5a49eb4cb8b3d817d2eefbb4c6645bbdda9fe852
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2019-11-14 14:35:01 -0800

    styled with styled-components

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -2,6 +2,79 @@ import React, {useState, useEffect} from 'react';
 import {withFormik, Form, Field} from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const CustomUserForm = styled.div`
+    background: #FEEBC8;
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+
+    @media (max-width: 600px){
+        flex-direction: column;
+        justify-content: flex-start;
+    }
+
+    Form, .userList{
+        width: 40%;
+        padding: 10px 10px;
+
+        @media (max-width: 600px){
+            width: 90%;
+        }
+    }
+
+    Form{
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        
+    }
+
+    .fieldWithError{
+        height: 50px;
+    }
+
+    h2{
+        margin: 15px 0px;
+    }
+
+    .textInput{
+        width: 100%;
+    }
+
+    input{
+        font-size: 1rem;
+    }
+
+    .error{
+        position: relative;
+        top: -18px;
+        color:red;
+    }
+
+    button{
+        margin-top: 15px;
+        width:8rem;
+        padding: 5px 5px;
+        background: #B2F5EA;
+        font-size: 1.0rem;
+    }
+
+    li{
+        margin: 10px 0;
+    }
+
+    ul{
+        border: 1px solid grey;
+        min-height: 100px;
+    }
+
+    .email{
+        color: grey;
+    }
+`
 
 const UserForm = ({values, errors, touched, status}) => {
     const [users, setUsers] = useState([]);
@@ -11,34 +84,48 @@ const UserForm = ({values, errors, touched, status}) => {
     }, [status])
 
     return(
-        <div>
+        <CustomUserForm>
             <Form>
-                <Field type='text' name='name' placeholder='Name'/>
-                {touched.name && errors.name && <span>{errors.name}</span>}
-                <br />
-                <Field type='email' name='email' placeholder='Email'/>
-                {touched.email && errors.email && <span>{errors.email}</span>}
-                <br />
-                <Field type='password' name='password' placeholder='Password'/>
-                {touched.password && errors.password && <span>{errors.password}</span>}
-                <br />
+                <h2>Add New User</h2>
+                <div className='fieldWithError'>
+                    <Field type='text' name='name' placeholder='Name' className='textInput'/>
+                    {touched.name && errors.name && <p className='error'>{errors.name}</p>}
+                </div>
+
+                <div className='fieldWithError'>
+                    <Field type='email' name='email' placeholder='Email' className='textInput'/>
+                    {touched.email && errors.email && <p className='error'>{errors.email}</p>}
+                </div>
+
+                <div className='fieldWithError'>
+                    <Field type='password' name='password' placeholder='Password' className='textInput'/>
+                    {touched.password && errors.password && <p className='error'>{errors.password}</p>}
+                </div>
+
                 <label>
-                    <Field type='checkbox' name='tos' checked={values.tos}/>
-                    Sign all your rights away.
+                    <span><Field type='checkbox' name='tos' checked={values.tos}/></span>
+                    <span>Sign all your rights away.</span>
                 </label>
-                <br />
-                <button type='submit'>Submit</button>
+
+                <button type='submit'><strong>Submit</strong></button>
             </Form>
             <div className='userList'>
+                <h2>User List</h2>
                 <ul>
-                {
+                    {
                     users.map(user => (
-                        <li>{user.name}</li>
+                        <li>
+                            <div>
+                                <span>{user.name}</span>
+                                <br/>
+                                <span className='email'>{user.email}</span>
+                            </div>
+                        </li>
                     ))
-                }
+                    }
                 </ul>
             </div>
-        </div>
+        </CustomUserForm>
     )
 }
 
