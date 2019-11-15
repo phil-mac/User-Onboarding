01930dc91807866584338342171f5bb80e1e1348
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2019-11-14 17:03:16 -0800

    improved readibility of code, moved styled-component styling to a css file

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -2,109 +2,36 @@ import React, {useState, useEffect} from 'react';
 import {withFormik, Form, Field} from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
-import styled from 'styled-components';
-
-const CustomUserForm = styled.div`
-    background: #FEEBC8;
-    height: 100vh;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-
-    @media (max-width: 600px){
-        flex-direction: column;
-        justify-content: flex-start;
-    }
-
-    Form, .userList{
-        width: 40%;
-        padding: 10px 10px;
-
-        @media (max-width: 600px){
-            width: 90%;
-        }
-    }
-
-    Form{
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-    }
-
-    .fieldWithError{
-        height: 60px;
-    }
-
-    h2{
-        margin: 15px 0px;
-    }
-
-    .textInput{
-        width: 100%;
-    }
-
-    input{
-        font-size: 1rem;
-    }
-
-    .error{
-        position: relative;
-        top: -18px;
-        color:red;
-    }
-
-    button{
-        margin-top: 15px;
-        width:8rem;
-        padding: 5px 5px;
-        background: #B2F5EA;
-        font-size: 1.0rem;
-    }
-
-    li{
-        margin: 10px 0;
-    }
-
-    ul{
-        border: 1px solid grey;
-        min-height: 100px;
-    }
-
-    .userInfo{
-        color: grey;
-    }
-
-    .roleSelect, label{
-        margin-bottom: 20px;
-    }
-`
 
 const UserForm = ({values, errors, touched, status}) => {
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([{
+        name: 'Jane',
+        email: 'jane@email.com',
+        role: 'Teacher',
+        color: 'green',
+        exp: 9
+    }]);
 
     useEffect(() => {
         status && setUsers([...users, status])
     }, [status])
 
     return(
-        <CustomUserForm>
+        <div className='formParent'>
             <Form>
                 <h2>Add New User</h2>
                 <div className='fieldWithError'>
                     <Field type='text' name='name' placeholder='Name' className='textInput'/>
                     {touched.name && errors.name && <p className='error'>{errors.name}</p>}
                 </div>
-
                 <div className='fieldWithError'>
                     <Field type='email' name='email' placeholder='Email' className='textInput'/>
                     {touched.email && errors.email && <p className='error'>{errors.email}</p>}
                 </div>
-
                 <div className='fieldWithError'>
                     <Field type='password' name='password' placeholder='Password' className='textInput'/>
                     {touched.password && errors.password && <p className='error'>{errors.password}</p>}
                 </div>
-
                 <div className='fieldWithError'>
                     <Field as='select' name='role' className='roleSelect'>
                         <option disabled value=''>Select a Role...</option>
@@ -115,50 +42,40 @@ const UserForm = ({values, errors, touched, status}) => {
                     </Field>
                     {touched.role && errors.role && <p className='error'>{errors.role}</p>}
                 </div>
-
                 <label>
-                    <span>Avatar Color:  </span>
-                    <span><Field type='color' name='color'/></span>
+                    Avatar Color:
+                    <Field type='color' name='color' value={values.color}/>
                 </label>
-
                 <label>
-                    <span>Profile Picture: </span>
-                    <span><Field type='file' name='new'/></span>
+                    Profile Picture <span className='warning'>(doesn't actually work)</span>:
+                    <Field type='file' name='new'/>
                 </label>
-
                 <label>
-                    <span>Experience Level (1-10): </span>
-                    <span><Field type='range' name="exp" min="0" max="10"/></span>
+                    Experience Level (1-10):
+                    <Field type='range' name="exp" min="0" max="10"/>
                 </label>
-
                 <label>
-                    <span><Field type='checkbox' name='tos' checked={values.tos}/></span>
-                    <span>Sign away all your rights.</span>
+                    <Field type='checkbox' name='tos' checked={values.tos}/>
+                    I love marketing emails, send me more.
                 </label>
-
                 <button type='submit'><strong>Submit</strong></button>
             </Form>
             <div className='userList'>
                 <h2>User List</h2>
                 <ul>
-                    {
-                    users.map(user => (
-                        <li>
+                    {users.map((user, index) => (
+                        <li key={index}>
                             <div>
-                                <span style={{color: `${user.color}`}}>{user.name}</span>
-                                <br/>
-                                <span className='userInfo'>{user.email}</span>
-                                <br/>
-                                <span className='userInfo'>{user.role}</span>
-                                <br/>
-                                <span className='userInfo'>Exp: {user.exp}</span>
+                                <h3 style={{color: `${user.color}`}}>{user.name}</h3>
+                                <p className='userInfo'>{user.email}</p>
+                                <p className='userInfo'>{user.role}</p>
+                                <p className='userInfo'>Level {user.exp}</p>
                             </div>
                         </li>
-                    ))
-                    }
+                    ))}
                 </ul>
             </div>
-        </CustomUserForm>
+        </div>
     )
 }
 
@@ -170,7 +87,7 @@ export default withFormik({
             password: password || '',
             role: role || '',
             tos: tos || true,
-            color: color,
+            color: color || "#0000ff",
             exp: exp || 5
         }
     },
